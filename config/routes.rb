2ca629dd1afb84e24d7abb8095cfc9fbb704393c
mum.t <mum.t.1003@gmail.com>
2020-09-08 08:06:28 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  root to: 'tasks#index'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  root to: 'toppages#index'
 
-get 'signup', to: 'users#new'
-  resources :tasks
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
 end
