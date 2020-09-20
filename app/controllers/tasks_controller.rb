0f69a2362d138b4c3d9c1edb20a98c09096ee49e
mum.t <mum.t.1003@gmail.com>
2020-09-20 13:28:08 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,10 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
+  before_action :correct_user, only: [:show, :edit, :destroy]
   
   def index
-    if logged_in?
-      @task = current_user.tasks.build  # form_with 用
+     
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
-    end
   end
 
   def show
